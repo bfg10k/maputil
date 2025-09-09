2986b93be8f6320e042d9c853b478ff76c773cf2
Lei Zhao <8961794+leizha@users.noreply.github.com>
2025-09-09 01:53:14 -0700

    rewrite

--- src/maputil/map2.py
+++ src/maputil/map2.py
@@ -1,6 +1,7 @@
 import sys
-import traceback
-from threading import Condition, Lock, Thread
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from threading import Lock
+from typing import Callable
 
 import pandas as pd
 
@@ -11,102 +12,66 @@ else:
 
 
 class Run:
-    inputs: list
-    results: list
+    _inputs: list
+    _results: list
 
     def __init__(self, fn, inputs, *, concurrency):
         if isinstance(inputs, pd.DataFrame):
-            self.inputs = inputs.to_dict(orient="records")
-            self.index = inputs.index
+            self._inputs = inputs.to_dict(orient="records")
+            self._index = inputs.index
         elif isinstance(inputs, pd.Series):
-            self.inputs = inputs.tolist()
-            self.index = inputs.index
+            self._inputs = inputs.tolist()
+            self._index = inputs.index
         elif isinstance(inputs, list):
-            self.inputs = inputs
-            self.index = None
+            self._inputs = inputs
+            self._index = None
         else:
-            assert False
+            raise TypeError("inputs must be a list, Series, or DataFrame")
 
-        assert isinstance(concurrency, int) and concurrency > 0
+        if not isinstance(concurrency, int) or concurrency <= 0:
+            raise ValueError("concurrency must be a positive integer")
         self.concurrency = concurrency
 
-        assert callable(fn)
-        self.fn = fn
+        if not callable(fn):
+            raise TypeError("fn must be a callable")
+        self._fn = fn
 
-        self.total = len(inputs)
-        self.results = [None] * self.total
-        self.nresults = 0
-        self.pending = list(range(self.total))
-        # pop() will remove from the end so reverse the pending array
-        self.pending.reverse()
+        self._total = len(self._inputs)
+        self._lock = Lock()
+        self._pbar = None
+        self._results = [None] * self._total
+        self._pending = set(range(self._total))
 
-        self.pbar = None
+    def _fn_wrapper(self, idx):
+        result = self._fn(self._inputs[idx])
+        with self._lock:
+            self._results[idx] = result
+            self._pending.remove(idx)
+            self._pbar.update()
 
-        self.err = None
-        self.nworkers = 0
-        self.lock = Lock()
-        self.cond = Condition(self.lock)
-
-    def worker(self):
+    def __call__(self):
+        pending = list(self._pending)
+        self._pbar = tqdm(total=self._total, initial=self._total - len(pending))
         try:
-            while True:
-                with self.lock:
-                    if self.err or not self.pending:
-                        break
-                    idx = self.pending.pop()
-                try:
-                    result = self.fn(self.inputs[idx])
-                    with self.lock:
-                        self.results[idx] = result
-                        self.nresults += 1
-                        self.pbar.update()
-                except Exception:
-                    with self.lock:
-                        # do not overwrite err
-                        if not self.err:
-                            self.err = traceback.format_exc()
-                        self.pbar.set_description("ERR")
-                    break
+            with ThreadPoolExecutor(max_workers=self.concurrency) as ex:
+                futs = [ex.submit(self._fn_wrapper, idx) for idx in pending]
+                for fut in as_completed(futs):
+                    fut.result()
+            return self.get_results()
         finally:
-            with self.lock:
-                self.nworkers -= 1
-                if self.nworkers == 0:
-                    self.pbar.close()
-                    self.pbar = None
-                    self.cond.notify_all()
+            self._pbar.close()
+            self._pbar = None
 
-    def start(self):
-        with self.lock:
-            if self.nworkers > 0:
-                return
-            npending = len(self.pending)
-            self.nworkers = nworkers = min(self.concurrency, npending)
-            self.pbar = tqdm(total=self.total, initial=self.total - npending)
+    def get_results(self, partial=False):
+        if not partial and self._pending:
+            raise ValueError("not all tasks have completed")
 
-        for _ in range(nworkers):
-            Thread(target=self.worker).start()
-
-    def join(self):
-        with self.cond:
-            while self.nworkers > 0:
-                self.cond.wait()
-
-    def get_results(self, partial=True):
-        with self.lock:
-            if not partial and self.nresults < self.total:
-                # if partial results is not desired, return None if we don't have all results yet
-                return None
-            results = self.results.copy()
-        if self.index is not None:
-            return pd.Series(results, index=self.index)
-        return results
-
-    def get_err(self):
-        with self.lock:
-            return self.err
+        if self._index is not None:
+            return pd.Series(self._results, index=self._index)
+        return self._results
 
 
-def map2(fn, inputs, *, concurrency=1):
-    r = Run(fn, inputs, concurrency=concurrency)
-    r.start()
-    return r
+def map2(
+    fn: Callable, inputs: list | pd.Series | pd.DataFrame, *, concurrency: int = 1
+) -> Run:
+    return Run(fn, inputs, concurrency=concurrency)
